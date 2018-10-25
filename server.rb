b7dc1873fa6d699fd5e3f8b722000aaac52fb4be
SidAvid <sid@avidsecure.io>
2018-10-25 10:04:05 +0530

    jahgsv

--- server.rb
+++ server.rb
@@ -206,9 +206,9 @@ class GHAapp < Sinatra::Application
         when 'file'
           logger.debug 'item is of type file'
 
-          # if item_name.end_with? '.tf'
-          #   logger.debug 'item is of type TF'
-          #   end
+          if item_name.end_with? '.tf'
+            logger.debug 'item is of type TF'
+          end
 
           # if item_name.ends_with? '.tf.json'
           #   logger.debug 'item is of type TF JSON'
